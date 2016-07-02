913355a6682fdb5b7102160fe440519c7fb43df7
Josh Hawkins <jkhaccounts@gmail.com>
2016-07-02 00:05:19 -0500

    Commit package.json starting template for index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var webdriverio  = require('webdriverio');
+
+module.exports = {
+    /**
+    * Logs the user in to Initium
+    *
+    * @param  {String} user
+    * @param  {String} pass
+    * @return {String}
+    */
+    login: function(user, pass) {
+        // TODO: Log in to initium
+    },
+
+    /**
+    * Say something in chat
+    *
+    * @param  {String} text
+    * @param  {String} channel
+    * @return {String}
+    */
+    say: function(text, channel) {
+        // TODO: Say something in chat channel
+    }
+};
